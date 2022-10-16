b56137c201367c238135592a0e251d54fe5979d3
Dark505000 <111893507+Dark505000@users.noreply.github.com>
2022-10-16 20:52:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='example_publish_pypi_medium',
+    name='eren',
     version='0.6',
     license='MIT',
     author="Giorgos Myrianthous",
-    author_email='email@example.com',
+    author_email='hhhuuuooo94@gmail.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/gmyrianthous/example-publish-pypi',
+    url='https://github.com/Dark505000/eren/edit/main/setup.py',
     keywords='example project',
     install_requires=[
           'scikit-learn',
